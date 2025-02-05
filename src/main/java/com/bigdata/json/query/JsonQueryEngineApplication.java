6854f160c2f39dca8e51bcd2e9b885c8dd290195
Eashan Roy <roy.ea@northeastern.edu>
2025-02-05 02:52:28 -0500

    chore: boilerplate code

---
+++ src/main/java/com/bigdata/json/query/JsonQueryEngineApplication.java
@@ -0,0 +1,13 @@
+package com.bigdata.json.query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonQueryEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonQueryEngineApplication.class, args);
+	}
+
+}
